ca5f7150cabebb1b98b699da15673d257618cda8
dmarques <diogohomemmarques@gmail.com>
2013-11-28 23:36:47 +0000

    awstats script style

--- install_awstats.sh
+++ install_awstats.sh
@@ -10,7 +10,7 @@ if [ $(id -u) -ne 0 ]; then
         exit 1
 fi
 
-# Obtain a user and password
+# obtain a user and password
 read -p "username: " username
 read -s -p "password: " password
 
@@ -21,46 +21,38 @@ sudo apt-get install -y awstats
 #nslookup $HOSTNAME | grep Name | sed 's/\s\+/ /g' | cut -d' ' -f2
 domain=$(nslookup $HOSTNAME | grep Name | cut -c7-)
 
-# Create a copy of awstats.conf for each domain:
+# create a copy of awstats.conf for the domain
 cp /etc/awstats/awstats.conf /etc/awstats/awstats.${domain}.conf
 
-# Modify the file:
+# awstats configuration
 sed -i "s/SiteDomain=\"\"/SiteDomain=\"${domain}\"/g" /etc/awstats/awstats.${domain}.conf
 sed -i "s/HostAliases=\"localhost 127.0.0.1\"/HostAliases=\"localhost 127.0.0.1 ${domain}\"/g" /etc/awstats/awstats.${domain}.conf
 
-# Generate the initial stats for AWStats based on existing var/log/apache2/access.log:
+# generate the initial stats for AWStats based on existing var/log/apache2/access.log:
 /usr/lib/cgi-bin/awstats.pl -config=${domain} -update
 
-
-# Apache configuration
-
-# Backup
+# backup apache configuration
 cp /etc/apache2/sites-available/default /etc/apache2/sites-available/"default-bak"$(date +"%Y-%m-%d:%s")
 
-# Add configurations to default
-apache_extra_config=" #AWStats config \n                                                     
+# create apache password file
+htpasswd -bc /etc/apache2/passwd ${username} ${password}
+
+# add apache directives to default
+apache_extra_config="#AWStats basic config \n
 Alias /awstatsclasses "/usr/share/awstats/lib/" \n
 Alias /awstats-icon "/usr/share/awstats/icon/" \n
 Alias /awstatscss "/usr/share/doc/awstats/examples/css" \n
 ScriptAlias /awstats/ /usr/lib/cgi-bin/ \n
-Options ExecCGI -MultiViews +SymLinksIfOwnerMatch  \n"
-
-sed -i "s,</VirtualHost>,$(echo $apache_extra_config) </VirtualHost>,g" /etc/apache2/sites-available/default
-
-# Password protection
-
-# create passwd file
-htpasswd -bc /etc/apache2/passwd ${username} ${password}
-
-
-# Add more configurations
-apache_extra_config="<Files "awstats.pl"> \n
+Options ExecCGI -MultiViews +SymLinksIfOwnerMatch \n
+# AWStats password protection« \n
+<Files "awstats.pl"> \n
 AuthUserFile /etc/apache2/passwd \n
 AuthName "Restricted Area" \n
 AuthType Basic \n
 require valid-user \n
 </Files> \n"
+
 sed -i "s,</VirtualHost>,$(echo $apache_extra_config) </VirtualHost>,g" /etc/apache2/sites-available/default
 
-# Reload apache
+# reload apache
 service apache2 restart
